0253ed191f375e88a7119fa8ba638694386bcc14
Hirad Asadi <hirad.asadi@gmail.com>
2020-12-20 09:54:13 +0100

    Reading configuration values now.

--- src/Program.cs
+++ src/Program.cs
@@ -1,5 +1,8 @@
+using System.IO;
+using System.Reflection;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 
 namespace ApiService
@@ -15,6 +18,7 @@ namespace ApiService
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseContentRoot(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location))
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
